9e7ef9eadaa7f7aacbfec8cac8de020ba2b3e7d9
caoshengxiang <1104729815@qq.com>
2019-06-17 19:46:53 +0800

    登录验证

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routerV from './components/routerV'
+import VueCookies from 'vue-cookies'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -18,6 +19,10 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: () => import('./views/login/login'),
+      meta: {
+        title: '登录',
+        authKey: false,
+      },
     },
     {
       path: '/yjy',
@@ -28,35 +33,75 @@ export default new Router({
           path: 'hexiao/Home',
           name: 'hexiaoHome',
           component: () => import('./views/hexiao/Home'),
+          meta: {
+            title: '活动核销',
+            authKey: true,
+          },
         }, {
           path: 'hexiao/detail',
           name: 'hexiaoDetail',
           component: () => import('./views/hexiao/detail'),
+          meta: {
+            title: '报名详情',
+            authKey: true,
+          },
         }, {
           path: 'history/detail',
           name: 'historyDetail',
           component: () => import('./views/hexiao/history'),
+          meta: {
+            title: '核销记录',
+            authKey: true,
+          },
         }, {
-          path: 'about',
-          name: 'about',
-          // route level code-splitting
-          // this generates a separate chunk (about.[hash].js) for this route
-          // which is lazy-loaded when the route is visited.
-          component: () => import('./views/About.vue')
-        },
-        {
           path: '*',
           name: '404',
           // redirect: '/',
-          component: () => import('./views/404/404.vue')
+          component: () => import('./views/404/404.vue'),
+          meta: {
+            title: '404',
+          },
         },
-      ]
+      ],
     },
     {
       path: '*',
       name: '404',
       // redirect: '/',
-      component: () => import('./views/404/404.vue')
+      component: () => import('./views/404/404.vue'),
+      meta: {
+        title: '404',
+      },
     },
-  ]
+  ],
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title // 在路由里面写入的meta里面的title字段
+  } else {
+    document.title = '遂企云'
+  }
+  if (to.meta.authKey) {
+    let token = VueCookies.get('token')
+    if (token) {
+      next()
+    } else {
+    next({
+      path: '/login',
+      query: {
+        backUrl: to.fullPath,
+      }, // 将跳转的路由path作为参数，登录成功后跳转到该路由
+    })
+  }
+} else {
+    next()
+  }
+})
+
+// 路由切换时回到页面顶部
+router.afterEach((to, from, next) => {
+  window.scrollTo(0, 0)
+})
+
+export default router
