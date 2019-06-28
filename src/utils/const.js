9567bcebbe7e6d0b0803b8d346798a64ac0922ca
caoshengxiang <1104729815@qq.com>
2019-06-28 15:41:09 +0800

    添加环境变量

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,4 @@
-// export const serverUrl = process.env.SERVER_BASE_URL // server地址
-// export const serverUrl = 'https://weixin.yanjiyou.net/vip'
-export const serverUrl = 'https://wx.yanjiyou.net/vip'
+export const serverUrl = process.env.VUE_APP_SERVER_BASE_URL
 
-console.log(serverUrl, 'test----环境地址')
+console.log(process.env.VUE_APP_ENV)
+console.log(process.env.VUE_APP_NAME)
